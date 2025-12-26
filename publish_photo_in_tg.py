114039642e4e0c2e28111d2186154a9a15091561
KamenskiyIlya <raptor5501999@gmail.com>
2025-12-26 10:50:40 +0300

    Добавил параметры при вызове из терминала в publish_photo_in_tg

--- publish_photo_in_tg.py
+++ publish_photo_in_tg.py
@@ -1,6 +1,9 @@
 import telegram
 import urllib3
 from environs import Env
+import argparse
+
+from help_scripts import post_photo_in_tg
 
 
 env = Env()
@@ -9,6 +12,18 @@ env.read_env()
 CHAT_ID = env.str('CHAT_ID')
 BOT = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
 
+def get_photo_name():
+	parser = argparse.ArgumentParser(description='Post photo in telegram')
+	parser.add_argument(
+		'-n',
+		'--photo_name',
+		default=None,
+		help='Укажите имя файл, если хотите хотите запостить конкретное фото с указанием разрешения'
+	)
+	args = parser.parse_args()
+	return args
+
 if __name__ == '__main__':
-	post_photo_in_tg(CHAT_ID, BOT)
+	args = get_photo_name()
+	post_photo_in_tg(CHAT_ID, BOT, args.photo_name)
 
